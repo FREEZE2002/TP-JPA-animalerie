804c0869a1161395bac707b34763800c0b90a05b
FREEZE2002 <almamoun.mahfoudi@gmail.com>
2023-02-22 23:31:11 +0100

    jpa-animalerie

---
+++ src/main/java/com/example/jpaanimalerie/JpaAnimalerieApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaanimalerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAnimalerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAnimalerieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaanimalerie/JpaAnimalerieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaanimalerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAnimalerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
